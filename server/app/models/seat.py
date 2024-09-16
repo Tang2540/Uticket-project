c31c15fdef8e700f43cd39d2544f9a623eb111ee
Pathanin Mapunya <pataninmapanya@gmail.com>
2024-09-16 14:29:14 +0700

    add current ver.

--- server/app/models/seat.py
+++ server/app/models/seat.py
@@ -5,5 +5,6 @@ class Seat(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     seatid: int
     seatPosition: Optional[str]
+    is_vacant: bool = Field(default=True)
     zone_id: Optional[int] = Field(default=None, foreign_key="zone.id")
         
