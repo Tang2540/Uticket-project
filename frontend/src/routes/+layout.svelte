d32f2def2c9ab41b47a487bdfd623339fdd45fd3
Pathanin Mapunya <pataninmapanya@gmail.com>
2024-11-01 22:42:04 +0700

    add frontend

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<nav>
+    <a href="/">home</a>
+    <a href="/blog">blog</a>
+</nav>
+<slot/>
+<footer>
+    <p>this is footer</p>
+</footer>
---
+++ frontend/src/routes/blog/+page.server.js
@@ -0,0 +1,7 @@
+import { posts } from "./data.js";
+
+export function load() {
+  return {
+    summaries: posts.map((post) => ({ slug: post.slug, title: post.title })),
+  };
+}
